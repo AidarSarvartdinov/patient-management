115d0480a8cbe4b392dad165930b3ae3b2093eca
Aidar <aidarsarvartdinov2004@gmail.com>
2026-01-13 18:27:26 +0500

    feat(patient): add update patient endpoint

--- patient-service/patient-service/src/main/java/com/pm/patient_service/dto/PatientRequestDTO.java
+++ patient-service/patient-service/src/main/java/com/pm/patient_service/dto/PatientRequestDTO.java
@@ -1,8 +1,9 @@
 package com.pm.patient_service.dto;
 
+import com.pm.patient_service.dto.validators.CreatePatientValidationGroup;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class PatientRequestDTO {
@@ -20,7 +21,7 @@ public class PatientRequestDTO {
     @NotBlank(message = "Date of birth is required")
     private String dateOfBirth;
 
-    @NotNull(message = "registered data is required")
+    @NotBlank(groups = CreatePatientValidationGroup.class, message = "registered data is required")
     private String registeredDate;
 
     public String getName() {
