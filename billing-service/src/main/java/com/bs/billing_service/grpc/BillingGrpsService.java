2ef0b4c0ae7cb89e37328ce1364165145b89aa3d
Aidar <aidarsarvartdinov2004@gmail.com>
2026-01-19 11:36:53 +0300

    feat(billing): add billing microservice

---
+++ billing-service/src/main/java/com/bs/billing_service/grpc/BillingGrpsService.java
@@ -0,0 +1,31 @@
+package com.bs.billing_service.grpc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpsService extends BillingServiceImplBase {
+    Logger log = LoggerFactory.getLogger(BillingGrpsService.class);
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+            StreamObserver<BillingResponse> responseObserver) {
+
+        log.info("createBillingAccount request received: {}", billingRequest.toString());
+
+        // business logic
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("1234")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
