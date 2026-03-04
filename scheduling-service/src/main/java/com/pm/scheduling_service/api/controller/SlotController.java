b9bc5ffd0d41b6f5c3ae3f936ea99a67e14f2037
Aidar <aidarsarvartdinov2004@gmail.com>
2026-03-04 11:25:09 +0300

    feat(scheduling): add swagger api documentation

--- scheduling-service/src/main/java/com/pm/scheduling_service/api/controller/SlotController.java
+++ scheduling-service/src/main/java/com/pm/scheduling_service/api/controller/SlotController.java
@@ -11,10 +11,13 @@ import com.pm.scheduling_service.api.dto.CreateSlotRequest;
 import com.pm.scheduling_service.api.dto.ReserveSlotRequest;
 import com.pm.scheduling_service.domain.service.SlotService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/slots")
+@Tag(name = "Slot", description = "API for managing time slots")
 public class SlotController {
     private final SlotService slotService;
 
@@ -23,12 +26,14 @@ public class SlotController {
     }
 
     @PostMapping
+    @Operation(summary = "Creates a new time slot for doctor")
     public ResponseEntity<Void> createSlot(@Valid @RequestBody CreateSlotRequest request) {
         slotService.createSlot(request.doctorId(), request.startTime(), request.endTime(), request.price());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PostMapping("/reserve")
+    @Operation(summary = "Reserve a slot for patient")
     public ResponseEntity<String> reserveSlot(@RequestBody ReserveSlotRequest request) {
         String sessionUrl = slotService.reserveSlot(request.slotId(), request.patientId());
         return ResponseEntity.ok(sessionUrl);
